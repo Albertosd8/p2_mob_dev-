14c3c250bdc713a3d218cc3362436b7a6512ebd7
Alberto Sandoval <55755198+Albertosd8@users.noreply.github.com>
2022-11-17 19:29:55 -0600

    Update songs_bloc.dart

--- lib/home/bloc_songs/songs_bloc.dart
+++ lib/home/bloc_songs/songs_bloc.dart
@@ -20,12 +20,15 @@ class FavsSongsBloc extends Bloc<SongsEvent, FavsSongsState> {
     on<RemoveFromSongsFavs>(_removeFromFavsSongs);
   }
 
-  Future<FutureOr<void>> _loadFavsSongs(event, emit) async {
+  Future<FutureOr<void>> _loadFavsSongs(event, emit) async {  //error going on firebase to load
       emit(LoadingSongs());
       try{
         String? uId = await FirebaseAuth.instance.currentUser?.uid;
-        CollectionReference favorites = await FirebaseFirestore.instance.collection("favorites");
-        var document = await favorites.doc(uId).get();
+        CollectionReference favorites = await FirebaseFirestore.instance.collection('favorites');
+        // print(favorites.doc(uId).get());
+        // var document = await favorites.doc(uId).get();
+        var document = await favorites.doc('favorites').get();
+        // print(document['hG5KIQM2pyjzlP9Stftw']); //
         if(document["favorites"].length == 0){
           emit(NoSongs());
         }else{
@@ -39,11 +42,15 @@ class FavsSongsBloc extends Bloc<SongsEvent, FavsSongsState> {
   FutureOr<void> _addToFavsSongs(event, emit) async{
     String? uId = await FirebaseAuth.instance.currentUser?.uid;
     CollectionReference favorites = await FirebaseFirestore.instance.collection("favorites");
-    var document = await favorites.doc(uId).get();
+
+    // var document = await favorites.doc('uId').get(); // .doc(uId) para el login pero no implementado//old line
+    var document = await favorites.doc("favorites").get(); //modified line
+
+    // print(document.data()); //documento en colección
 
     if(await document.data() == null){
       try{
-        favorites.doc(uId).set({
+        favorites.doc("favorites").set({
           "favorites": [
             {
               'image': event.newFavSong['image'],
@@ -58,11 +65,13 @@ class FavsSongsBloc extends Bloc<SongsEvent, FavsSongsState> {
       }
     }else{
       var list_songs = document.get('favorites');
+      // print("songs list on document");print(list_songs); //songs in document favorites
       if(list_songs.any((element) => DeepCollectionEquality().equals(element, {
         'image': event.newFavSong['image'],
         'song_name': event.newFavSong['song_name'],
         'author': event.newFavSong['author'],
       }))) {
+        // print("duplicated song");
         emit(DuplicateSongs());
       }else{
         list_songs.add({
@@ -70,24 +79,26 @@ class FavsSongsBloc extends Bloc<SongsEvent, FavsSongsState> {
           'song_name': event.newFavSong['song_name'],
           'author': event.newFavSong['author'],
         });
-        favorites.doc(uId).set({
+        favorites.doc('favorites').set({
           "favorites": list_songs,
         });
         emit(SuccessfulAddedToSongs());
       }
     }
-    // Map<String,String> new_fav = { // por hacer
-    //   "image": event.newFavSong['image'],
-    //   "song_name": event.newFavSong['song_name'],
-    //   "author": event.newFavSong['author']
-    // };
-    // if(songs_list.contains(new_fav)){
-    //   print("ya estoy en la lista");
-    //   emit(SuccessfulAddedToSongs());
-    // }else{
-    //   print("me añadi a la lista");
-    //   songs_list.add(new_fav);
-    //   emit(SuccessfulAddedToSongs());
-    // }
   }
 
+  FutureOr<void> _removeFromFavsSongs(event, emit) async {
+    String? uId = await FirebaseAuth.instance.currentUser?.uid;
+    CollectionReference favorites = FirebaseFirestore.instance.collection('favorites');
+    var document = await favorites.doc('favorites').get();
+    var list_songs = await document.get('favorites');
+    final index = list_songs.indexWhere((element) =>
+      element["song_name"] == event.songToRemove["song_name"] &&
+      element["song_artist"] == event.songToRemove["song_artist"]);
+    list_songs.removeAt(index);
+    favorites.doc('favorites').set({
+      "favorites": list_songs,
+    });
+    emit(SuccessfulRemovedFromSongs());
+  }
+}
